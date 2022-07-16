e89d1d72c9057066a2722d3ccef5b24600e3b2b4
Daniel Chicchon <danielchicchon@gmail.com>
2022-07-16 15:34:15 -0700

    b

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log('test');
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/test-vite/",
